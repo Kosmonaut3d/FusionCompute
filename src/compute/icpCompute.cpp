c3d7c5dad67b73e772cfa31b1c2d7e7b3845b17d
Thomas Swierkot <thomas.swierkot@hm.edu>
2022-03-21 10:59:20 +0100

    ICP Compute basics

--- src/compute/icpCompute.cpp
+++ src/compute/icpCompute.cpp
@@ -3,11 +3,11 @@
 
 //----------------------------------------------------------------------------------------------------------
 ICPCompute::ICPCompute(glm::vec3 origin, int resolution, float scale)
-    : m_computeSDFShader{}
+    : m_computeICPShader{}
     , m_texID{}
 {
-	m_computeSDFShader.setupShaderFromFile(GL_COMPUTE_SHADER, "resources/computeSDF.comp");
-	m_computeSDFShader.linkProgram();
+	m_computeICPShader.setupShaderFromFile(GL_COMPUTE_SHADER, "resources/computeSDF.comp");
+	m_computeICPShader.linkProgram();
 
 	setupTexture();
 }
@@ -28,134 +28,20 @@ void ICPCompute::setupTexture()
 }
 
 //----------------------------------------------------------------------------------------------------------
-void ICPCompute::compute(unsigned int pointCloudId, unsigned int pointCloudNormalId, glm::mat4x4& viewToWorld,
-                         glm::mat4x4 worldToClipKinect)
+void ICPCompute::compute(unsigned int newVertexWorldTex)
 {
-	GLuint   query;
-	GLuint64 elapsed_time;
+	m_computeICPShader.begin();
 
-	if (GUIScene::SceneSelection::SDF == GUIScene::s_sceneSelection)
-	{
-		glGenQueries(1, &query);
-		glBeginQuery(GL_TIME_ELAPSED, query);
-	}
+	// m_computeICPShader.setUniform1f("_zeroPlaneDistInv", 1.0 / m_planeDist);
+	// m_computeICPShader.setUniform1f("_zeroPixelSizeDouble", 2. * m_pixelSize);
+	// glBindImageTexture(0, depthTexID, 0, GL_FALSE, 0, GL_READ_ONLY, GL_R32F);
+	glBindImageTexture(1, newVertexWorldTex, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
 
-	if (GUIScene::s_sdfResolution != m_resolution)
-	{
-		m_resolution = GUIScene::s_sdfResolution;
-		glDeleteTextures(1, &m_texID);
-		setupTexture();
-	}
-
-	glm::vec3 point = GUIScene::s_testPointPos;
-
-	glm::vec3 pointLocal = m_modelMatInv * glm::vec4(point, 1);
-
-	// Make the truncation distance dependent on the minimum distance between 2 tiles
-	const float truncationScaled = m_scale / m_resolution * GUIScene::s_sdfTruncation;
-
-	m_computeSDFShader.begin();
-
-	m_computeSDFShader.setUniform1f("_stepSize", 1. / m_resolution);
-	m_computeSDFShader.setUniformMatrix4f("_pclWorldToClip", worldToClipKinect);
-	m_computeSDFShader.setUniformMatrix4f("_viewToWorld", viewToWorld);
-	m_computeSDFShader.setUniformMatrix3f("_viewToWorldRot", glm::mat3(viewToWorld));
-	m_computeSDFShader.setUniformMatrix4f("_modelMatrix", m_modelMat);
-	m_computeSDFShader.setUniform1f("_truncationDistance", truncationScaled);
-	m_computeSDFShader.setUniform1f("_maxWeight", GUIScene::s_sdfWeightTruncation);
-
-	glBindImageTexture(0, pointCloudId, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
-	glBindImageTexture(1, pointCloudNormalId, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
-	glBindImageTexture(2, m_texID, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RG32F);
-
-	m_computeSDFShader.dispatchCompute(m_resolution, m_resolution, m_resolution);
-	m_computeSDFShader.end();
-	/*
-	std::vector<glm::vec2> framedata(m_resolution * m_resolution * m_resolution);
-	glBindTexture(GL_TEXTURE_3D, m_texID);
-	glGetTextureImage(m_texID, 0, GL_RED, GL_FLOAT, framedata.size() * sizeof(glm::vec2), &framedata[0]);
-	*/
-
-	if (GUIScene::SceneSelection::SDF == GUIScene::s_sceneSelection)
-	{
-		glEndQuery(GL_TIME_ELAPSED);
-		glGetQueryObjectui64v(query, GL_QUERY_RESULT, &GUIScene::s_measureGPUTime);
-	}
+	m_computeICPShader.dispatchCompute(640, 480, 1);
+	m_computeICPShader.end();
 }
 
-//----------------------------------------------------------------------------------------------------------
-unsigned int ICPCompute::getTextureID()
+unsigned int ICPCompute::getTexID()
 {
 	return m_texID;
-}
-
-//----------------------------------------------------------------------------------------------------------
-glm::mat4x4& ICPCompute::getWorldInv()
-{
-	return m_modelMatInv;
-}
-
-//----------------------------------------------------------------------------------------------------------
-void ICPCompute::drawOutline()
-{
-	ofPushStyle();
-	ofPushMatrix();
-
-	ofGetCurrentRenderer()->setFillMode(ofFillFlag::OF_OUTLINE);
-	ofSetColor(200, 100, 200);
-	ofMultMatrix(m_modelMat);
-	ofNoFill();
-
-	ofSetColor(200, 0, 0);
-	ofDrawBox(glm::vec3(.5, .5, .5), 1);
-
-	ofPopMatrix();
-	ofPopStyle();
-}
-
-//----------------------------------------------------------------------------------------------------------
-void ICPCompute::drawRaymarch(ofCamera& camera)
-{
-	GLuint   query;
-	GLuint64 elapsed_time;
-
-	if (GUIScene::SceneSelection::SDF == GUIScene::s_sceneSelection)
-	{
-		glGenQueries(1, &query);
-		glBeginQuery(GL_TIME_ELAPSED, query);
-	}
-
-	// glEnable(GL_CULL_FACE);
-	// glCullFace(GL_BACK);
-
-	ofPushStyle();
-	m_raymarchSDFShader.begin();
-	// glPolygonMode(GL_BACK, GL_LINE);
-	float scalehalf = m_scale / 2;
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_3D, m_texID);
-
-	// Make the truncation distance dependent on the minimum distance between 2 tiles
-	const float truncationScaled = m_scale / m_resolution * GUIScene::s_sdfTruncation;
-
-	m_raymarchSDFShader.setUniform3f("cameraWorld", camera.getPosition());
-	m_raymarchSDFShader.setUniformMatrix4f("sdfBaseTransform", m_modelMatInv);
-	m_raymarchSDFShader.setUniform1f("sdfResolution", m_resolution);
-	m_raymarchSDFShader.setUniform1f("_truncationDistance", truncationScaled);
-	m_raymarchSDFShader.setUniformMatrix4f("viewprojection", camera.getModelViewProjectionMatrix());
-	m_raymarchSDFShader.setUniform1f("near", camera.getNearClip());
-	m_raymarchSDFShader.setUniform1f("far", camera.getFarClip());
-
-	ofDrawBox(m_origin + glm::vec3(scalehalf, scalehalf, scalehalf), m_scale, m_scale, m_scale);
-	m_raymarchSDFShader.end();
-	ofPopStyle();
-
-	// glDisable(GL_CULL_FACE);
-
-	if (GUIScene::SceneSelection::SDF == GUIScene::s_sceneSelection)
-	{
-		glEndQuery(GL_TIME_ELAPSED);
-		glGetQueryObjectui64v(query, GL_QUERY_RESULT, &GUIScene::s_measureGPUTime2);
-	}
 }
--- src/compute/icpCompute.h
+++ src/compute/icpCompute.h
@@ -6,11 +6,11 @@ class ICPCompute
 {
   public:
 	ICPCompute(glm::vec3 origin, int resolution, float scale);
-	void setupTexture();
-	void compute(unsigned int pointCloudId, unsigned int pointCloudNormalId, glm::mat4x4& viewToWorld,
-	             glm::mat4x4 worldToClipKinect);
+	void         setupTexture();
+	void         ICPCompute::compute(unsigned int newVertexWorldTex);
+	unsigned int getTexID();
 
   private:
-	ofShader     m_computeSDFShader;
+	ofShader     m_computeICPShader;
 	unsigned int m_texID;
 };
